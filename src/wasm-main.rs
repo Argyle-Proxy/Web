682369bf72e0ef1a4b9b21e7d87d1d0bb143f854
Quartinal <148902345+Quartinal@users.noreply.github.com>
2023-12-17 10:09:28 -0800

    Updates on wasm-main.rs

--- src/wasm-main.rs
+++ src/wasm-main.rs
@@ -1,1 +1,12 @@
+use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+extern "C" {
+  fn alert(s: &str)
+}
+
+// Just a sample component
+#[wasm_bindgen]
+pub fn greet() {
+  alert("Hi, there! We're glad you landed. If you're feeling comfortable, please consider pressing Ok.")
+}
